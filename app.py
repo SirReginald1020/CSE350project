b8e7693d7f6c5e4ae2bbb12bdee573f9fa170098
SirReginald1020 <126435944+SirReginald1020@users.noreply.github.com>
2023-12-02 07:26:30 -0500

    App and Index with my graph
    Implemented Google Graph, has cool features and displays from the table I made in a weird but functional way.

--- app.py
+++ app.py
@@ -4,9 +4,26 @@ import pandas as pd
 import os
 import shutil
 from kaggle.api.kaggle_api_extended import KaggleApi
-
+from io import BytesIO
+import base64
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+
+from flask import render_template
+from flask_mail import Mail, Message
+
+mail = Mail(app)
+
+@app.errorhandler(500)
+def internal_error(error):
+    # Send an email to your support email with the error details
+    msg = Message('Internal Server Error', sender='cmcarp05@louisville.edu', recipients=['ccarpenter0527@gmail.com'])
+    msg.body = str(error)
+    mail.send(msg)
+    return render_template('500.html'), 500
 
 from flask import Response
 
@@ -77,6 +94,56 @@ def cut_csv(filein, fileout, colstokeep):
                     del row[col]
                 writer.writerow(row)
 
+    # To make the graph use avg_prices_peryear.csv
+    fpath = 'dataset-folder/avg_prices_peryear.csv'
+    years = []
+    cost_1 = []
+    cost_2 = []
+
+    with open(fpath, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            years.append(row['Year'])
+            cost_1.append(row['Annual Fuel Cost (FT1)'])
+            cost_2.append(row['Annual Fuel Cost (FT2)'])
+    chart_data = "['Year', 'Annual Cost (FT1)', 'Annual Cost (FT2)'],\n"
+    for i in range(len(years)):
+        chart_data += f"['{years[i]}', {cost_1[i]}, {cost_2[i]}], \n"
+    existing_html = 'templates/page1.html'
+    with open(existing_html, 'r') as existing:
+        html_content = existing.read()
+
+    placeholder = '<!-- placeholder for chart -->'
+    chartscript = f'''
+    google.charts.load('current', {{"packages":["corechart"]}}); 
+
+    google.charts.setOnLoadCallback(drawChart); 
+
+    function drawChart() {{ 
+
+        var data = google.visualization.arrayToDataTable([ 
+
+            {chart_data} 
+
+        ]); 
+
+        var options = {{ 
+
+            title: 'Average of Annual Fuel Costs Over Years', 
+
+            curveType: 'function', 
+
+            legend: {{ position: 'bottom' }} 
+
+        }}; 
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart')); 
+
+        chart.draw(data, options); 
+    }} '''
+    html_content = html_content.replace(placeholder, chartscript)
+    with open(existing_html, 'w') as existing:
+        existing.write(html_content)
 
 # Function that sets up the Kaggle API connection and authenticates
 def connect_to_kaggle():
@@ -114,33 +181,75 @@ def get_csv_from_kaggle():
                 'Annual Fuel Cost (FT2)']
         cut_csv('database.csv', 'database.csv', keep)
         data = pd.read_csv(filepath)
-
-        # Make the average table by reading the data, grouping by year and mathing,
-        # move it to data folder
-        readavg = pd.read_csv('dataset-folder/database.csv')
-        avg_prices = readavg.groupby('Year')[['Annual Fuel Cost (FT1)','Annual Fuel Cost (FT2)']].mean()
-        avg_prices.to_csv('avg_prices_peryear.csv', index=True)
-        movencheck = os.path.join('dataset-folder', 'avg_prices_peryear.csv')
-        if os.path.exists(movencheck):
-            os.remove(movencheck)
-        shutil.move('avg_prices_peryear.csv', 'dataset-folder')
         return data
     else:
         print(f"File {filepath} not found. Check if the dataset contains 'database.csv'.")
         return None
 
+from flask import request
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # Get the search query and filter criteria from the form
+    search_query = request.form.get('search_query')
+    filter_fuel_type1 = request.form.get('filter_fuel_type1')
+    filter_fuel_type2 = request.form.get('filter_fuel_type2')
+
+    # Get the data
+    fuel_data = get_csv_from_kaggle()
+
+    # If the data is successfully retrieved, convert it to records for display
+    if fuel_data is not None:
+        if search_query:
+            # Filter data based on the search query (modify this logic as needed)
+            fuel_data = fuel_data[fuel_data['Make'].str.contains(search_query, case=False) |
+                                  fuel_data['Model'].str.contains(search_query, case=False)]
+        
+        if filter_fuel_type1:
+            # Filter data by Fuel Type 1
+            fuel_data = fuel_data[fuel_data['Fuel Type 1'] == filter_fuel_type1]
+
+        if filter_fuel_type2:
+            # Filter data by Fuel Type 2
+            fuel_data = fuel_data[fuel_data['Fuel Type 2'] == filter_fuel_type2]
+
+        data_records = fuel_data.to_dict(orient='records')
+        return render_template('index.html', data=data_records, search_query=search_query)
+    else:
+        return render_template('error.html', message="Data could not be loaded.")
+
+@app.route('/page1')
+def page1():
     # Get the data
     fuel_data = get_csv_from_kaggle()
 
     # If the data is successfully retrieved, convert it to records for display
     if fuel_data is not None:
         data_records = fuel_data.to_dict(orient='records')
-        return render_template('/index.html', data=data_records)
+
+        # Create the chart
+        plt.figure(figsize=(8, 4))
+        sorted_data = fuel_data.groupby('Fuel Type 1')['Annual Fuel Cost (FT1)'].mean().sort_values(ascending=False)
+        sorted_data.plot(kind='bar', color='skyblue')
+        plt.title('Average Annual Fuel Cost (FT1) by Fuel Type 1')
+        plt.xlabel('Fuel Type 1')
+        plt.ylabel('Average Annual Fuel Cost (FT1)')
+        plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
+        plt.tight_layout()
+
+        # Save the plot to a BytesIO object
+        img = BytesIO()
+        plt.savefig(img, format='png')
+        img.seek(0)
+
+        # Convert the image to base64 for embedding in HTML
+        img_str = base64.b64encode(img.read()).decode('utf-8')
+
+        # Pass the data and image to the template
+        return render_template('page1.html', data_records=data_records, img_str=img_str)
     else:
         return render_template('error.html', message="Data could not be loaded.")
+
 
 
 if __name__ == '__main__':
@@ -159,4 +268,3 @@ if __name__ == '__main__':
         app.run(debug=True)
     if not debug:
         app.run()
-
